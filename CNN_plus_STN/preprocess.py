39831e5c8b61f636f44c939170fa729072fb888d
namasmankad <25651233+namasmankad@users.noreply.github.com>
2021-12-17 21:56:41 -0500

    Add files via upload

---
+++ CNN_plus_STN/preprocess.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+import numpy as np
+
+from tqdm import tqdm
+
+root_path = '../input/german_traffic_sign/GTSRB/Final_Training/Images/'
+
+all_paths = os.listdir(root_path)
+all_paths.sort()
+data = pd.DataFrame()
+labels = []
+counter = 0
+
+for i, path in tqdm(enumerate(all_paths), total=len(all_paths)):
+    all_images = os.listdir(root_path+all_paths[i])
+    for image in all_images:
+        if image.split('.')[1] == 'ppm':
+            image_name = image.split('.')[0]
+            data.loc[counter, 'image_path'] = f"{root_path}{all_paths[i]}/{image_name}"
+            data.loc[counter, 'label'] = i
+            counter += 1
+
+data = data.sample(frac=1).reset_index(drop=True)
+data.to_csv('../input/german_traffic_sign/GTSRB/data.csv', index=False)
+
+print(data.head(5))
